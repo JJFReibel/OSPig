7280c231eb8b45a48ff4b32b48e1e4162fa4dd9e
Jean-Jacques François Reibel <9219877+JJFReibel@users.noreply.github.com>
2021-08-16 13:40:34 -0700

    Uploaded OSPig.py

---
+++ OSPig.py
@@ -0,0 +1,14 @@
+# By JJ Reibel, using Python 3.10
+import platform
+
+# Get Computer Name
+print("Computer Name:"+ " "*19 + str(platform.node()))
+
+# Get OS
+print(str("Operating System:" + " "*16 + platform.system()))
+
+# Get OS Release
+print(str("Operating System Release:" + " "*8 + platform.release()))
+
+# Get OS Version
+print(str("Operating System Version:" + " "*8 + platform.platform()))
